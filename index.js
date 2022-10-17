4439d41749f86716c85f64c0e5b0e0b9aa44880e
Andre Dragacevac <a.dragacevac@icloud.com>
2022-10-17 18:48:34 -0400

    commit tree 7e9aefd67bd834f9f9b0c11c05230e5551d69b8a

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution in this file!
+function returnFirstTwoDrivers(){
+    const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+return drivers.slice(0,2)
+}
+function returnLastTwoDrivers(){
+    const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+return drivers.slice(-2)
+}
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+selectingDrivers;
+
+function createFareMultiplier(num){
+    return function(number){
+        return num*number
+    }
+}
+function fareDoubler(num){
+    return num*2
+}
+function fareTripler(num){
+    return num*3
+}
+ function selectDifferentDrivers(drivers, returnFirstTwoDrivers){
+    return returnFirstTwoDrivers(drivers);
+ }
